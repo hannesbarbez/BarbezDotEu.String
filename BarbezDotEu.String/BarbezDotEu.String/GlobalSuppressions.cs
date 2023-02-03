fda90d524063992583d188da1c7e8d03ada4ceb5
Hannes Barbez <hannesbarbez@hotmail.com>
2023-02-03 18:03:12 +0100

    SuppressMessage

---
+++ BarbezDotEu.String/BarbezDotEu.String/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Hannes Barbez. All rights reserved.
+// Licensed under the GNU General Public License v3.0
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("GeneratedRegex", "SYSLIB1045:Convert to 'GeneratedRegexAttribute'.")]
